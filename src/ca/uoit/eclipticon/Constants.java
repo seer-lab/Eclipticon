6789bf40eac2b5f6122f3acb2a5b8eaaff5864d3
kevinjalbert <kevin.j.jalbert@gmail.com>
2010-03-13 15:25:24 -0500

    * Changed the Constans class to fix how the constants are represented * The syntax to check for in the source code has been adjusted to correctly identify Java synchronization constructs

--- src/ca/uoit/eclipticon/Constants.java
+++ src/ca/uoit/eclipticon/Constants.java
@@ -2,32 +2,44 @@ package ca.uoit.eclipticon;
 
 /**
  * This is a constants class that holds the constants being used throughout
- * the project. All the constants are of type Integer to simplify the use through
- * the project. The constants are used for the various pieces of information for
- * the instrumentation data.
+ * the project. These constants hold the string place-holders of the synchronization
+ * constructs as well as the syntax that will indicate an interest point, there
+ * is also a representation of the type of instrumentation point.
  * 
  * @author Chris Forbes, Kevin Jalbert, Cody LeBlanc
  */
 public final class Constants {
 
 	/** These are the types of synchronization constructs */
-	static public int		SYNCHRONIZE			= 0;
-	static public int		BARRIER				= 1;
-	static public int		LATCH				= 2;
-	static public int		SEMAPHORE			= 3;
+	static public String	SYNCHRONIZE							= "synchronize";
+	static public String	LATCH								= "latch";
+	static public String	BARRIER								= "barrier";
+	static public String	SEMAPHORE							= "semaphore";
 
-	/** These are the syntax representation of synchronization constructs */
-	static public String	SYNCHRONIZESYNTAX	= "synchronized";
-	static public String	BARRIERSYNTAX		= ".signal()";
-	static public String	LATCHSYNTAX			= ".lock()";
-	static public String	SEMAPHORESYNTAX		= ".acquire()";
+	/** These are the syntax representation of synchronized constructs */
+	static public String	SYNCHRONIZE_BLOCK					= "synchronized";
+	static public String	SYNCHRONIZE_LOCK					= ".lock";
+	static public String	SYNCHRONIZE_LOCKINTERRUPTIBLY		= ".lockInterruptibly";
+	static public String	SYNCHRONIZE_TRYLOCK					= ".tryLock";
+	static public String	SYNCHRONIZE_UNLOCK					= ".unlock";
+	static public String	SYNCHRONIZE_NEWCONDITION			= ".newCondition";
 
-	/** These are the types of instrumentation modes */
-	static public int		MANUAL				= 0;
-	static public int		AUTOMATIC			= 1;
-	static public int		BOTH				= 2;
+	/** These are the syntax representation of latch constructs */
+	static public String	LATCH_COUNTDOWN						= ".countdown";
+	static public String	LATCH_AWAIT							= ".await";
+
+	/** These are the syntax representation of barrier constructs */
+	static public String	BARRIER_RESET						= ".reset";
+	static public String	BARRIER_AWAIT						= ".await";
+
+	/** These are the syntax representation of semaphore constructs */
+	static public String	SEMAPHORE_ACQUIRE					= ".acquire";
+	static public String	SEMAPHORE_ACQUIREUNINTERRUPTIBLY	= ".acquireUninterruptibly";
+	static public String	SEMAPHORE_TRYACQUIRE				= ".tryAcquire";
+	static public String	SEMAPHORE_DRAIN						= ".drainPermits";
+	static public String	SEMAPHORE_RELEASE					= ".release";
 
 	/** These are the types of noise */
-	static public int		SLEEP				= 0;
-	static public int		YEILD				= 1;
+	static public int		SLEEP								= 0;
+	static public int		YEILD								= 1;
 }
